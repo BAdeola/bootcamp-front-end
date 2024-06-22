65fdaa9ca64a7e736472112cdecc4321668728b4
Brayan Adeola <brayanadeola@gmail.com>
2024-06-21 21:46:34 -0300

    Andamento do projeto de Cilco de Vida

--- React/ciclo-de-vida/src/App.jsx
+++ React/ciclo-de-vida/src/App.jsx
@@ -1,24 +1,27 @@
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = { showCounter: false } //é uma variável para mostrar ou não o conteúdo
+  }
+
+  //setState({  }) = atualiza uma variável
+  // tudo que esteja entre '{  }' é um código em javascript dentro do react
+  //this.state.showCounter ? 'Remover Contador' : 'Mostrar Contador' = esse tipo de sintaxe é um operador ternário
+  render() {
+    return (
+      <div>
+        <h1>Ciclo de Vida no React</h1>
+
+        <button onClick={() => {
+          this.setState({ showCounter: !this.state.showCounter });
+        }}>{ this.state.showCounter ? 'Remover Contador' : 'Mostrar Contador' }</button> {/* caso o estado da variável seja verdadeiro, irá mostrar 'Remover Contador', caso contrário 'Mostrar Contador' */}
+      </div>
+    );
+  }
 }
 
 export default App;
